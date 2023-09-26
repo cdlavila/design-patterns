841471a6178bb761dfe9cef27c46d6f1d664c6f3
Carlos <carlosdaniel.londono@utp.edu.co>
2023-09-26 12:48:40 -0500

    Factory challenge

--- creational/factory/challenge.ts
+++ creational/factory/challenge.ts
@@ -1,6 +1,5 @@
 // 1
 interface HttpAdapter {
-    framework: string;
     get(): void;
     post(): void;
     put(): void;
@@ -9,67 +8,91 @@ interface HttpAdapter {
 }
 
 // 2
-class RestHttpAdapter implements HttpAdapter {
-    framework: string;
-
-    constructor(type: string) {
-        this.framework = type;
-    }
-
+class ExpressHttpAdapter implements HttpAdapter {
     /** @implements */
     get(): void {
-        console.log('GET using ' + this.framework);
+        console.log('GET using Express');
     }
 
     /** @implements */
     post(): void {
-        console.log('POST using ' + this.framework);
+        console.log('POST using Express');
     }
 
     /** @implements */
     put(): void {
-        console.log('PUT using ' + this.framework);
+        console.log('PUT using Express');
     }
 
     /** @implements */
     patch(): void {
-        console.log('PATCH using ' + this.framework);
+        console.log('PATCH using Express');
     }
 
     /** @implements */
     delete(): void {
-        console.log('DELETE using ' + this.framework);
+        console.log('DELETE using Express');
+    }
+}
+
+class FastifyHttpAdapter implements HttpAdapter {
+    /** @implements */
+    get(): void {
+        console.log('GET using Fastify');
+    }
+
+    /** @implements */
+    post(): void {
+        console.log('POST using Fastify');
+    }
+
+    /** @implements */
+    put(): void {
+        console.log('PUT using Fastify');
+    }
+
+    /** @implements */
+    patch(): void {
+        console.log('PATCH using Fastify');
+    }
+
+    /** @implements */
+    delete(): void {
+        console.log('DELETE using Fastify');
     }
 }
 
 // 3
 interface HttpAdapterFactory {
-    makeAdapter(framework: string): HttpAdapter;
+    makeAdapter(): HttpAdapter;
 }
 
 // 4
-class RestHttpAdapterFactory implements HttpAdapterFactory {
+class ExpressHttpAdapterFactory implements HttpAdapterFactory {
     /** @implements */
-    makeAdapter(type: string): HttpAdapter {
-        return new RestHttpAdapter(type);
+    makeAdapter(): HttpAdapter {
+        return new ExpressHttpAdapter();
     }
 }
 
-// test
-((): void => {
-    const factory: HttpAdapterFactory = new RestHttpAdapterFactory();
+class FastifyHttpAdapterFactory implements HttpAdapterFactory {
+    /** @implements */
+    makeAdapter(): HttpAdapter {
+        return new FastifyHttpAdapter();
+    }
+}
 
-    const expressHttpAdapter: HttpAdapter = factory.makeAdapter('Express');
-    expressHttpAdapter.get();
-    expressHttpAdapter.post();
-    expressHttpAdapter.put();
-    expressHttpAdapter.patch();
-    expressHttpAdapter.delete();
+// App
+function appFactory(factory: HttpAdapterFactory): void {
+    const httpAdapter: HttpAdapter = factory.makeAdapter();
+    httpAdapter.get();
+    httpAdapter.post();
+    httpAdapter.put();
+    httpAdapter.patch();
+    httpAdapter.delete();
+}
 
-    const fastifyHttpAdapter: HttpAdapter = factory.makeAdapter('Fastify');
-    fastifyHttpAdapter.get();
-    fastifyHttpAdapter.post();
-    fastifyHttpAdapter.put();
-    fastifyHttpAdapter.patch();
-    fastifyHttpAdapter.delete();
-})()
+console.log('--- Express ---');
+appFactory(new ExpressHttpAdapterFactory());
+console.log('--- Fastify ---');
+appFactory(new FastifyHttpAdapterFactory());
