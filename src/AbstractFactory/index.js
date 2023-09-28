d6c2535af3d6901b994272e8c1c3ab36fa39ecf0
Carlos <carlosdaniel.londono@utp.edu.co>
2023-09-28 17:06:27 -0500

    Add Abstract Factory

---
+++ src/AbstractFactory/index.js
@@ -0,0 +1,200 @@
+/**
+ * Abstract Factory Design Pattern
+ *
+ * Intent: Lets you produce families of related objects without specifying their
+ * concrete classes.
+ */
+
+/**
+ * Each distinct product of a product family should have a base interface.
+ * All variants of the product must implement this interface.
+ */
+class AbstractProductA {
+  /**
+   * @abstract
+   */
+  usefulFunctionA() {
+    throw new Error('usefulFunctionA() method not implemented!');
+  }
+}
+
+/**
+ * These Concrete Products are created by corresponding Concrete Factories.
+ */
+class ConcreteProductA1 extends AbstractProductA {
+  /**
+   * @override
+   */
+  usefulFunctionA() {
+    return 'The result of the product A1.';
+  }
+}
+
+class ConcreteProductA2 extends AbstractProductA {
+  /**
+   * @override
+   */
+  usefulFunctionA() {
+    return 'The result of the product A2.';
+  }
+}
+
+/**
+ * Here's the base interface of another product.
+ * All products can
+ * interact with each other, but proper interaction is possible only between
+ * products of the same concrete variant.
+ */
+class AbstractProductB {
+  /**
+   * @abstract
+   * Product B is able to do its own thing...
+   */
+  usefulFunctionB() {
+    throw new Error('usefulFunctionB() method not implemented!');
+  }
+
+  /**
+   * @abstract
+   * ...but it also can collaborate with the ProductA.
+   * The Abstract Factory makes sure that all products it creates are of the
+   * same variant and thus, compatible.
+   */
+  anotherUsefulFunctionB(collaborator) {
+    throw new Error('anotherUsefulFunctionB() method not implemented!');
+  }
+}
+
+/**
+ * These Concrete Products are created by corresponding Concrete Factories.
+ */
+class ConcreteProductB1 extends AbstractProductB {
+  /**
+   * @override
+   */
+  usefulFunctionB() {
+    return 'The result of the product B1.';
+  }
+
+  /**
+   * @override
+   * The variant, Product B1, is only able to work correctly with the
+   * variant, Product A1.
+   * Nevertheless, it accepts any instance of
+   * AbstractProductA as an argument.
+   */
+  anotherUsefulFunctionB(collaborator) {
+    const result = collaborator.usefulFunctionA();
+    return `The result of the B1 collaborating with the (${result})`;
+  }
+}
+
+class ConcreteProductB2 extends AbstractProductB {
+  /**
+   * @override
+   */
+  usefulFunctionB() {
+    return 'The result of the product B2.';
+  }
+
+  /**
+   * @override
+   * The variant, Product B2, is only able to work correctly with the
+   * variant, Product A2.
+   * Nevertheless, it accepts any instance of
+   * AbstractProductA as an argument.
+   */
+  anotherUsefulFunctionB(collaborator) {
+    const result = collaborator.usefulFunctionA();
+    return `The result of the B2 collaborating with the (${result})`;
+  }
+}
+
+/**
+ * The Abstract Factory interface declares a set of methods that return
+ * different abstract products. These products are called a family and are
+ * related to a high-level theme or concept. Products of one family are usually
+ * able to collaborate among themselves. A family of products may have several
+ * variants, but the products of one variant are incompatible with products of
+ * another.
+ */
+class AbstractFactory {
+  /**
+   * @abstract
+   */
+  createProductA() {
+    throw new Error('createProductA() method not implemented!');
+  }
+
+  /**
+   * @abstract
+   */
+  createProductB() {
+    throw new Error('createProductB() method not implemented!');
+  }
+}
+
+/**
+ * Concrete Factories produce a family of products that belong to a single
+ * variant. The factory guarantees that resulting products are compatible. Note
+ * that signatures of the Concrete Factory's methods return an abstract product,
+ * while inside the method a concrete product is instantiated.
+ */
+class ConcreteFactory1 extends AbstractFactory {
+  /**
+   * @override
+   */
+  createProductA() {
+    return new ConcreteProductA1();
+  }
+
+  /**
+   * @override
+   */
+  createProductB() {
+    return new ConcreteProductB1();
+  }
+}
+
+/**
+ * Each Concrete Factory has a corresponding product variant.
+ */
+class ConcreteFactory2 extends AbstractFactory {
+  /**
+   * @override
+   */
+  createProductA() {
+    return new ConcreteProductA2();
+  }
+
+  /**
+   * @override
+   */
+  createProductB() {
+    return new ConcreteProductB2();
+  }
+}
+
+/**
+ * The client code works with factories and products only through abstract
+ * types: AbstractFactory and AbstractProduct. This lets you pass any factory or
+ * product subclass to the client code without breaking it.
+ */
+function clientCode(factory) {
+  const productA = factory.createProductA();
+  const productB = factory.createProductB();
+
+  console.log(productB.usefulFunctionB());
+  console.log(productB.anotherUsefulFunctionB(productA));
+}
+
+/**
+ * The client code can work with any concrete factory class.
+ */
+console.log('Client: Testing client code with the first factory type...');
+clientCode(new ConcreteFactory1());
+
+console.log('');
+
+console.log('Client: Testing the same client code with the second factory type...');
+clientCode(new ConcreteFactory2());
